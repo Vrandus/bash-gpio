93a7f686840fd034f3ae84d0ad3f4cdd173ee288
Vrandus <lolcrosell@gmail.com>
2018-11-19 14:54:54 +0000

    fixed cylon times

--- cylon.sh
+++ cylon.sh
@@ -2,15 +2,15 @@
 let "fourth = 1 / 4"
 for (( ; ; ))
 do
-	for i in 1 2 4 8
+	for i in 1 2 4 
 	do
 		./setbits.sh $i
-		sleep $fourth
+		sleep 0.25s
 	done
-
-	for i in 8 4 2 1
+	
+	for i in 8 4 2 
 	do
 		./setbits.sh $i
-		sleep $fourth
+		sleep 0.25s
 	done
 done
